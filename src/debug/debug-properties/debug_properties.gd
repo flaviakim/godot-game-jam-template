f642f969d38941fa108fa58995fe1a8c8eee9920
Manuel Geissberger <manuel.geissberger@gmail.com>
2026-02-02 19:00:08 +0100

    refactor

--- src/debug/debug-properties/debug_properties.gd
+++ src/debug/debug-properties/debug_properties.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready() -> void:
 	if !OS.has_feature("editor"): hide()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_released("toggle_debug"):
 		if visible: hide()
 		else: show()
