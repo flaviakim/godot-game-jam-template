0e75a64d3a166dd2f66dceeda60e5b01ea2fe586
Manuel Geissberger <manuel.geissberger@gmail.com>
2026-02-06 11:45:12 +0100

    minimallistic pause menu

---
+++ src/in-game-ui/in-game-controller.gd
@@ -0,0 +1,24 @@
+class_name InGameController extends CanvasLayer
+
+@onready var pause_btn: Button = $Control/PauseButton
+@onready var pause_menu: CanvasLayer = $"../PauseGameUi"
+@onready var world: Node = $"../world"
+
+var is_paused: bool = false:
+	set(value):
+		is_paused = value
+		if is_paused:
+			hide()
+			pause_menu.show()
+			world.process_mode = Node.PROCESS_MODE_DISABLED
+		else:
+			show()
+			pause_menu.hide()
+			world.process_mode = Node.PROCESS_MODE_INHERIT
+
+func _ready() -> void:
+	pause_btn.button_up.connect(func(): is_paused = !is_paused)
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_released("pause_game"):
+		is_paused = !is_paused
